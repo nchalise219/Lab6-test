d63772b8b49ecb89de6aa795ff27cf26ddcdd6e6
cadbia <cbialow@ufl.edu>
2023-10-25 14:23:04 -0400

    added decoder

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+def decoder(encoded_password):
+    # Initialize an empty string to store the decoded password
+    decoded = ''
+
+    # Loop through each character in the encoded password
+    for char in encoded_password:
+        # Check if the character is a digit
+        if char.isdigit():
+            # Convert the digit to an integer, subtract 3, take modulo 10 to handle underflow, and add back to the decoded string
+            decoded += str((int(char) - 3) % 10)
+
+    return decoded
+
+
 def encode(number):
     for int in number:
         int += 3
@@ -10,15 +24,16 @@ def print_menu():
     print("1. Encode")
     print("2. Decode")
     print("3. Quit")
-def main():
-   select = int(input("Please enter an option: "))
 
-   if select == 1:
-       password = int(input("Please enter your password to encode: "))
-       encode(password)
-       print("Your password has been encoded and stored!")
+
+def main():
+    select = int(input("Please enter an option: "))
+
+    if select == 1:
+        password = int(input("Please enter your password to encode: "))
+        encode(password)
+        print("Your password has been encoded and stored!")
+
 
 if __name__ == '__main__':
     main()
-
-
